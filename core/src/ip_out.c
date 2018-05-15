9fc1d8183d6d40b66e0b18ea244a6f03ef36aa1c
xiaodongyi <409070169@qq.com>
2018-05-15 15:50:50 +0800

    check arp

--- core/src/ip_out.c
+++ core/src/ip_out.c
@@ -43,7 +43,7 @@ ForwardIPPacket(mtcp_manager_t mtcp, struct pkt_ctx *pctx)
 	struct iphdr *iph;
 	uint32_t daddr = 0;
 
-	if (g_config.mos->nic_forward_table != NULL) {
+	/*if (g_config.mos->nic_forward_table != NULL) {
 		pctx->out_ifidx = 
 			g_config.mos->nic_forward_table->nic_fwd_table[pctx->p.in_ifidx];
 		if (pctx->out_ifidx != -1) {
@@ -51,7 +51,7 @@ ForwardIPPacket(mtcp_manager_t mtcp, struct pkt_ctx *pctx)
 		    printf("Haddr:%s\n",haddr);
 			goto fast_tx;
 		}
-	}
+	}*/
 
 	/* set daddr for easy code writing */
 	daddr = pctx->p.iph->daddr;		
