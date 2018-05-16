66faca9ac841dbd08ce2348588f58f9caa06da31
xiaodongyi <409070169@qq.com>
2018-05-16 18:43:00 +0800

    check arp

--- core/src/dpdk_module.c
+++ core/src/dpdk_module.c
@@ -241,19 +241,19 @@ dpdk_send_pkts(struct mtcp_thread_context *ctxt, int nif)
 		}
 #endif /* !ENABLE_STATS_IOCTL */
 #endif
-		printf("cnt:%d\n",cnt);
+		/*printf("cnt:%d\n",cnt);
 		if(cnt==1){
 		    struct ethhdr *ethh =(struct ethhdr *)rte_pktmbuf_mtod(pkts[0], struct ethhdr *);
 		    printf("in dpdk send pkt!!!!\n");
 		    printf("source Haddr:%x:%x:%x:%x:%x:%x\n",ethh->h_source[0],ethh->h_source[1],ethh->h_source[2],ethh->h_source[3],ethh->h_source[4],ethh->h_source[5]);
 		    printf("dst Haddr:%x:%x:%x:%x:%x:%x\n",ethh->h_dest[0],ethh->h_dest[1],ethh->h_dest[2],ethh->h_dest[3],ethh->h_dest[4],ethh->h_dest[5]);
 		}
-		printf("nif:%d\n",nif);
+		printf("nif:%d\n",nif);*/
 		do {
 			/* tx cnt # of packets */
 			ret = rte_eth_tx_burst(nif, qid, 
 					       pkts, cnt);
-			printf("ret: %d\n",ret);
+			//printf("ret: %d\n",ret);
 			pkts += ret;
 			cnt -= ret;
 			/* if not all pkts were sent... then repeat the cycle */
