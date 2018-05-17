9bd16a85d93939528b2cc5d6c081639b7cad4a4a
xiaodongyi <409070169@qq.com>
2018-05-17 11:52:24 +0800

    check arp

--- core/src/dpdk_module.c
+++ core/src/dpdk_module.c
@@ -378,9 +378,10 @@ dpdk_recv_pkts(struct mtcp_thread_context *ctxt, int ifidx)
 	
 	ret = rte_eth_rx_burst((uint8_t)ifidx, qid,
 			       dpc->pkts_burst, MAX_PKT_BURST);
-	printf("in receive pkt!!\n");
-	printf("ret:%d\n",ret);
+
 	if(ret!=0){
+	    printf("in receive pkt!!\n");
+	    printf("ret:%d\n",ret);
 	    struct ethhdr *ethh =(struct ethhdr *)rte_pktmbuf_mtod(dpc->pkts_burst[0], struct ethhdr *);
 	    printf("source Haddr:%x:%x:%x:%x:%x:%x\n",ethh->h_source[0],ethh->h_source[1],ethh->h_source[2],ethh->h_source[3],ethh->h_source[4],ethh->h_source[5]);
 	    printf("dst Haddr:%x:%x:%x:%x:%x:%x\n",ethh->h_dest[0],ethh->h_dest[1],ethh->h_dest[2],ethh->h_dest[3],ethh->h_dest[4],ethh->h_dest[5]);
